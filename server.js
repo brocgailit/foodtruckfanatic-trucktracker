b04d1d9445065cd3c7eb009da810c88dd7fe8a86
Broc Gailit <brocgailit@gmail.com>
2014-03-09 18:58:51 -0700

    Cross origin fixes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if ('development' === app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.all('/', function(req, res, next) {
+app.all('/trucks/location', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
